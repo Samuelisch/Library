ae6d42a1e600a556ddbfcbfe73be556ccd943c14
Samuelisch <samuelchanwk@hotmail.com>
2021-07-02 17:20:42 +0800

    created modules for localStorage and library, expanded functions for Book class

--- js/scriptTest.js
+++ js/scriptTest.js
@@ -1,3 +1,39 @@
+const storage = (() => {
+    //checks if local storage available
+    function storageAvailable(type) {
+        var storage;
+        try {
+            storage = window[type];
+            var x = '__storage_test__';
+            storage.setItem(x, x);
+            storage.removeItem(x);
+            return true;
+        }
+        catch(e) {
+            return e instanceof DOMException && (
+                // everything except Firefox
+                e.code === 22 ||
+                // Firefox
+                e.code === 1014 ||
+                // test name field too, because code might not be present
+                // everything except Firefox
+                e.name === 'QuotaExceededError' ||
+                // Firefox
+                e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+                // acknowledge QuotaExceededError only if there's something already stored
+                (storage && storage.length !== 0);
+        }
+    }
+
+    function clearCache() {
+        localStorage.clear();
+        for (let book in myLibrary) {
+            delete myLibrary[book];
+        }
+        bookCells.forEach(cell => cell.remove());
+    }
+})();
+
 const form = (() => {
     const infoCell = document.querySelector('.info'); //first cell in page - to initiate form
     const icon = document.querySelector('.add-prompt');
@@ -50,9 +86,23 @@ const form = (() => {
     };
 })();
 
-class Library {
-    //IIFE?
-}
+const library = (() => {
+    let myLibrary = [];
+
+    function loadBooks() { //from localStorage
+
+    }
+
+    function addBook() { //after form submit or from localStorage
+
+    }
+
+    function displayBooks(book) { //function from load book or add book
+
+    }
+
+
+})();
 
 class Book {
     constructor(title, author, pages, pagesRead) {
@@ -60,14 +110,6 @@ class Book {
         this.author = author;
         this.pages = pages;
         this.pagesRead = pagesRead;
-    }
-
-    addToLibrary() {
-
-    }
-
-    display() {
-
     }
 }
 
